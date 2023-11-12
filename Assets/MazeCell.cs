34aabc94beade1a94430e6d8beb414fcff86a9f9
gineres <giselle.neres.contact@gmail.com>
2023-11-11 21:59:43 -0300

    feat: maze generation working

--- Assets/MazeCell.cs
+++ Assets/MazeCell.cs
@@ -4,32 +4,46 @@ using UnityEngine;
 
 public class MazeCell : MonoBehaviour
 {
-    [SerializeField] private GameObject leftWall;
-    [SerializeField] private GameObject rightWall;
-    [SerializeField] private GameObject upWall;
-    [SerializeField] private GameObject downWall;
-    [SerializeField] private GameObject middleWall;
+    [SerializeField]
+    private GameObject _leftWall;
 
-    public bool IsVisited {get; private set;}
+    [SerializeField]
+    private GameObject _rightWall;
 
-    public void Visit(){
+    [SerializeField]
+    private GameObject _frontWall;
+
+    [SerializeField]
+    private GameObject _backWall;
+
+    [SerializeField]
+    private GameObject _unvisitedBlock;
+
+    public bool IsVisited { get; private set; }
+
+    public void Visit()
+    {
         IsVisited = true;
-        middleWall.SetActive(false);
+        _unvisitedBlock.SetActive(false);
     }
 
-    public void ClearLeftWall(){
-        leftWall.SetActive(false);
+    public void ClearLeftWall()
+    {
+        _leftWall.SetActive(false);
     }
 
-    public void ClearRightWall(){
-        rightWall.SetActive(false);
+    public void ClearRightWall()
+    {
+        _rightWall.SetActive(false);
     }
 
-    public void ClearUpWall(){
-        upWall.SetActive(false);
+    public void ClearFrontWall()
+    {
+        _frontWall.SetActive(false);
     }
 
-    public void ClearDownWall(){
-        downWall.SetActive(false);
+    public void ClearBackWall()
+    {
+        _backWall.SetActive(false);
     }
 }
--- Assets/MazeGenerator.cs
+++ Assets/MazeGenerator.cs
@@ -1,32 +1,149 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
-    [SerializeField] private MazeCell mazeCell;
-    [SerializeField] private int mazeWidth;
-    [SerializeField] private int mazeHeight;
+    [SerializeField]
+    private MazeCell _mazeCellPrefab;
 
-    private MazeCell[,] mazeMap; 
+    [SerializeField]
+    private int _mazeWidth;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private int _mazeDepth;
+
+    private MazeCell[,] _mazeGrid;
+
     void Start()
     {
-        mazeMap = new MazeCell[mazeWidth, mazeHeight];
+        _mazeGrid = new MazeCell[_mazeWidth, _mazeDepth];
 
-        for (int x = 0; x < mazeWidth; x++)
+        for (int x = 0; x < _mazeWidth; x++)
         {
-            for (int y = 0; y < mazeHeight; y++)
+            for (int y = 0; y < _mazeDepth; y++)
             {
-                mazeMap[x,y] = Instantiate(mazeCell, new Vector2(x + ((mazeWidth - 1)/-2f),y + ((mazeHeight - 1)/-2f)), Quaternion.identity);
+                _mazeGrid[x, y] = Instantiate(_mazeCellPrefab, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+
+        GenerateMaze(null, _mazeGrid[0, 0]);
+
+        for (int x = 0; x < _mazeWidth; x++)
+        {
+            for (int y = 0; y < _mazeDepth; y++)
+            {
+                _mazeGrid[x, y].transform.position = new Vector2(x + ((_mazeWidth-1)/-2f),y  + ((_mazeDepth-1)/-2f));
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateMaze(MazeCell previousCell, MazeCell currentCell)
     {
-        
+        currentCell.Visit();
+        ClearWalls(previousCell, currentCell);
+
+        MazeCell nextCell;
+
+        do
+        {
+            nextCell = GetNextUnvisitedCell(currentCell);
+
+            if (nextCell != null)
+            {
+                GenerateMaze(currentCell, nextCell);
+            }
+        } while (nextCell != null);
     }
+
+    private MazeCell GetNextUnvisitedCell(MazeCell currentCell)
+    {
+        var unvisitedCells = GetUnvisitedCells(currentCell);
+
+        return unvisitedCells.OrderBy(_ => Random.Range(1, 10)).FirstOrDefault();
+    }
+
+    private IEnumerable<MazeCell> GetUnvisitedCells(MazeCell currentCell)
+    {
+        int x = (int)currentCell.transform.position.x;
+        int y = (int)currentCell.transform.position.y;
+
+        if (x + 1 < _mazeWidth)
+        {
+            var cellToRight = _mazeGrid[x + 1, y];
+            
+            if (cellToRight.IsVisited == false)
+            {
+                yield return cellToRight;
+            }
+        }
+
+        if (x - 1 >= 0)
+        {
+            var cellToLeft = _mazeGrid[x - 1, y];
+
+            if (cellToLeft.IsVisited == false)
+            {
+                yield return cellToLeft;
+            }
+        }
+
+        if (y + 1 < _mazeDepth)
+        {
+            var cellToFront = _mazeGrid[x, y + 1];
+
+            if (cellToFront.IsVisited == false)
+            {
+                yield return cellToFront;
+            }
+        }
+
+        if (y - 1 >= 0)
+        {
+            var cellToBack = _mazeGrid[x, y - 1];
+
+            if (cellToBack.IsVisited == false)
+            {
+                yield return cellToBack;
+            }
+        }
+    }
+
+    private void ClearWalls(MazeCell previousCell, MazeCell currentCell)
+    {
+        if (previousCell == null)
+        {
+            return;
+        }
+
+        if (previousCell.transform.position.x < currentCell.transform.position.x)
+        {
+            previousCell.ClearRightWall();
+            currentCell.ClearLeftWall();
+            return;
+        }
+
+        if (previousCell.transform.position.x > currentCell.transform.position.x)
+        {
+            previousCell.ClearLeftWall();
+            currentCell.ClearRightWall();
+            return;
+        }
+
+        if (previousCell.transform.position.y < currentCell.transform.position.y)
+        {
+            previousCell.ClearFrontWall();
+            currentCell.ClearBackWall();
+            return;
+        }
+
+        if (previousCell.transform.position.y > currentCell.transform.position.y)
+        {
+            previousCell.ClearBackWall();
+            currentCell.ClearFrontWall();
+            return;
+        }
+    }
+
 }
