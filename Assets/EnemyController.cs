7065028eff10f07c5a0cff5c9b92e54bff687e0a
gineres <giselle.neres.contact@gmail.com>
2023-11-13 03:48:20 -0300

    feat: game loop

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class EnemyController : Tree
 {
     Pathfinding pathfinding;
-    private float moveSpeed = 1f;
 
     protected override BehaviourNode SetupTree(){
-        //BehaviourNode root = new EnemyTaskWait();
         BehaviourNode root = new Selector(new List<BehaviourNode>{
             new Sequence(new List<BehaviourNode>{
                 new CheckPlayerInRange(pathfinding),
@@ -23,28 +21,4 @@ public class EnemyController : Tree
     {
         pathfinding = GetComponent<Pathfinding>();
     }
-    
-    /*
-
-    void Update()
-    {
-        /* LOGICA DE SEGUIR O JOGADOR
-        if (pathfinding.foundPath != null)
-        {
-            if (pathfinding.foundPath.Count > 0)
-            {
-                Vector3 direction = pathfinding.foundPath[0].WorldPosition - transform.position;
-                transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
-            }
-        }*/
-
-        /*
-        if (pathfinding.foundPath != null)
-        {
-            if (pathfinding.foundPath.Count > 20)
-            {
-                
-            }
-        }
-    }*/
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
 
+    private int fullHp = 100;
+    private int hp;
+    private int bombAmount = 3;
+
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private Text bombText;
+
+    private float attackRadius = 2f;
+    public LayerMask enemyLayer;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        hp = fullHp;
+        UpdateHealthBar();
+        UpdateBombText();
     }
 
     void Update()
@@ -19,5 +35,94 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
         movement.Normalize();
         rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
+        DrawCircle(.01f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PerformAttack();
+            bombAmount--;
+            UpdateBombText();
+        }
+    }
+
+    void PerformAttack()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, attackRadius, enemyLayer);
+        
+
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Destroy(collider.gameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Collectible")
+        {
+            Collectible collectible = other.gameObject.GetComponent<Collectible>();
+            if (collectible.collectibleType == CollectibleType.HEAL)
+            {
+                hp += 20;
+                if (hp > fullHp)
+                {
+                    hp = fullHp;
+                }
+                UpdateHealthBar();
+            } else
+            {
+                bombAmount++;
+                UpdateBombText();
+            }
+
+            Destroy(other.gameObject);
+        } else if (other.tag == "Enemy")
+        {
+            hp -= 20;
+            if (hp <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            UpdateHealthBar();
+        } else if (other.tag == "Exit")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (hpSlider != null)
+        {
+            hpSlider.value = hp;
+        }
+    }
+
+    void UpdateBombText(){
+        bombText.text = "Bomb: " + bombAmount.ToString();
+    }
+
+    public void DrawCircle(float lineWidth)
+    {
+        var segments = 360;
+        var line = GetComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.startWidth = lineWidth;
+        line.endWidth = lineWidth;
+        line.positionCount = segments + 1;
+
+        var pointCount = segments + 1; 
+        var points = new Vector3[pointCount];
+
+        for (int i = 0; i < pointCount; i++)
+        {
+            var rad = Mathf.Deg2Rad * (i * 360f / segments);
+            points[i] = new Vector3(Mathf.Sin(rad) * attackRadius, Mathf.Cos(rad) * attackRadius, 0);
+        }
+
+        line.SetPositions(points);
     }
 }
